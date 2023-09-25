da107ec69fba71d2e3bb24219585805d60d98d7f
Spencer Elliott <s.elliott123@gmail.com>
2023-09-25 13:49:30 -0500

    Added ability to disable profiling

--- src/main.c
+++ src/main.c
@@ -15,6 +15,8 @@ KOS_INIT_FLAGS(INIT_DEFAULT);
 extern uint8 romdisk[];
 KOS_INIT_ROMDISK(romdisk);
 
+#define DO_PROFILING 0
+
 int main(int argc, char** argv) {
     glKosInit();
 
@@ -25,19 +27,25 @@ int main(int argc, char** argv) {
 
     dEngine_Init(GL_11_RENDERER, 640, 480);
 
+#if DO_PROFILING
     uint32 count = 0;
 
     profiler_init("/pc/dEngine_prof.out");
     profiler_start();
     while (count < 10) {
+#else
+    while(1) {
         dEngine_HostFrame();
 
         pvr_stats_t stats;
         pvr_get_stats(&stats);
 
         printf("FPS: %f\n", stats.frame_rate);
+    #if DO_PROFILING
         count++;
+    #endif
     }
+#endif
     profiler_stop();
 
     return 0;
